5d7b759de47a624e8d10e6cf4dd6d5687d13bfd6
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-10 22:48:57 -0300

    Método GET

--- src/main/java/br/com/fiap/trabalhoFinal/repository/ApiRepository.java
+++ src/main/java/br/com/fiap/trabalhoFinal/repository/ApiRepository.java
@@ -2,42 +2,64 @@ package br.com.fiap.trabalhoFinal.repository;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
-import java.util.stream.Stream;
 
 import br.com.fiap.trabalhoFinal.dto.Statistic;
 import br.com.fiap.trabalhoFinal.dto.Transaction;
 
 public class ApiRepository {
 
-	private ArrayList<Transaction> transactions = new ArrayList<>();
-	private final int minute = 60000;
+	public ArrayList<Transaction> transactions = new ArrayList<>();
 	
 	public boolean save(Transaction transaction) {
 		
-		if (System.currentTimeMillis() - transaction.getTimestamp() <= minute) {			
+		if (System.currentTimeMillis() - transaction.getTimestamp() <= 60000) {			
 			transactions.add(transaction);
 			return true;			
 		}
 		
-		return false;
+		return false;        
     }
 	
 	public Statistic all() {
 		
 		Statistic statistic = new Statistic();
 		
-		DoubleStream ds = (DoubleStream) ((Stream<Transaction>) transactions
+		ArrayList<Transaction> filterTransactions = 
+				(ArrayList<Transaction>) transactions
 					.stream()
-					.filter(t -> System.currentTimeMillis() - t.getTimestamp() <= minute)
-					.collect(Collectors.toList()))
-					.mapToDouble(Transaction::getAmount);
-
-		statistic.setSum(ds.sum());
-		statistic.setAvg(ds.average().getAsDouble());
-		statistic.setMax(ds.max().getAsDouble());
-		statistic.setMin(ds.min().getAsDouble());
-		statistic.setCount((int) ds.count());
+					.filter(t -> System.currentTimeMillis() - t.getTimestamp() <= 60000)
+					.collect(Collectors.toList());
+		
+		double sum = filterTransactions
+				.stream()
+				.mapToDouble(Transaction::getAmount)
+				.sum();
+		
+		double avg = filterTransactions
+				.stream()
+				.mapToDouble(Transaction::getAmount)
+				.average()
+				.getAsDouble();
+		
+		double max = filterTransactions
+				.stream()
+				.mapToDouble(Transaction::getAmount)
+				.max()
+				.getAsDouble();
+		
+		double min = filterTransactions
+				.stream()
+				.mapToDouble(Transaction::getAmount)
+				.min()
+				.getAsDouble();
+		
+		int count = filterTransactions.size();
+		
+		statistic.setAvg(avg);
+		statistic.setSum(sum);
+		statistic.setMax(max);
+		statistic.setMin(min);
+		statistic.setCount(count);
 		
 		return statistic;
 	}
